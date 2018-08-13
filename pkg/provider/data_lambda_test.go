13a89d0007863812bc1c0fa24f1bcae69f6869a4
Eduardo Lopez <elopez@chanzuckerberg.com>
2018-08-13 14:27:13 -0700

    Fix template encoding + add test #18

--- pkg/provider/data_lambda_test.go
+++ pkg/provider/data_lambda_test.go
@@ -1,6 +1,9 @@
 package provider_test
 
 import (
+	"archive/zip"
+	"bufio"
+	"strings"
 	"testing"
 
 	r "github.com/hashicorp/terraform/helper/resource"
@@ -24,7 +27,7 @@ func TestLambdaCreate(t *testing.T) {
 
 				data "bless_lambda" "zip" {
 					encrypted_ca = "aaaa"
-					encrypted_password = "bbbb"
+					encrypted_password = "bb+bb"
 					service_name = "test"
 					kmsauth_key_id = "keyID"
 					output_path = "/tmp/test.zip"
@@ -32,7 +35,7 @@ func TestLambdaCreate(t *testing.T) {
 
 				data "bless_lambda" "zip2" {
 					encrypted_ca = "aaaa"
-					encrypted_password = "bbbb"
+					encrypted_password = "bb+bb"
 					service_name = "test"
 					kmsauth_key_id = "keyID"
 					output_path = "/tmp/test2.zip"
@@ -52,6 +55,8 @@ func TestLambdaCreate(t *testing.T) {
 					a.NotEmpty(output1)
 					a.NotEmpty(output2)
 					// Check hashes are equal
+
+					validateBlessConfig(t, "/tmp/test.zip")
 					a.Equal(output1, output2)
 					return nil
 				},
@@ -101,4 +106,39 @@ func TestLambdaCreate(t *testing.T) {
 			},
 		},
 	})
+}
+
+func validateBlessConfig(t *testing.T, zipPath string) {
+	a := assert.New(t)
+
+	r, err := zip.OpenReader(zipPath)
+	a.Nil(err)
+	defer r.Close()
+
+	configFound := false
+	privateKeyFound := false
+
+	for _, f := range r.File {
+		if f.Name != "bless_deploy.cfg" {
+			continue
+		}
+		configFound = true
+		fc, err := f.Open()
+		a.Nil(err)
+		defer fc.Close()
+		scanner := bufio.NewScanner(fc)
+
+		for scanner.Scan() {
+			if strings.Contains(scanner.Text(), "bb+bb") {
+				privateKeyFound = true
+				break
+			}
+		}
+		a.Nil(scanner.Err())
+
+		break // Found the config file
+	}
+
+	a.True(configFound)
+	a.True(privateKeyFound)
 }
